84701007609ad6b3a13d9db074f302d85345b9c4
jiangzheng24 <jiangzheng1@jd.com>
2019-07-22 15:58:28 +0800

    first commit

---
+++ src/main/java/com/jiangzh/archaeopteryx/archaeopteryxparent/ArchaeopteryxParentApplication.java
@@ -0,0 +1,13 @@
+package com.jiangzh.archaeopteryx.archaeopteryxparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchaeopteryxParentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArchaeopteryxParentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jiangzh/archaeopteryx/archaeopteryxparent/ArchaeopteryxParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jiangzh.archaeopteryx.archaeopteryxparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArchaeopteryxParentApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
